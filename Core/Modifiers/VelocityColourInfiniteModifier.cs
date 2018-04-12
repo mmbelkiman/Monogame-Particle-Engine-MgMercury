097292e35edca31191b2bc92f7600c12c2284d20
Marcelo Belkiman <marcelobelkiman@gmail.com>
2018-04-11 23:54:39 -0300

    New Modifier

---
+++ Core/Modifiers/VelocityColourInfiniteModifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MonoGameMPE.Core.Modifiers
+{
+    public class VelocityColourInfiniteModifier : IModifier
+    {
+        public Colour Colour1 { get; set; }
+        public Colour Colour2 { get; set; }
+        public float VelocityChangeColor { get; set; }
+
+        private int TimePass = 0;
+        private bool useColour1 = true;
+
+        public unsafe void Update(float elapsedSeconds, ParticleBuffer.ParticleIterator iterator)
+        {
+            if (TimePass < VelocityChangeColor) TimePass++;
+            else { TimePass = 0; }
+
+            if (TimePass >= VelocityChangeColor)
+            {
+                useColour1 = !useColour1;
+                TimePass = 0;
+            }
+
+            while (iterator.HasNext)
+            {
+                var particle = iterator.Next();
+                particle->Colour = useColour1 ? Colour1 : Colour2;
+            }
+        }
+    }
+}
