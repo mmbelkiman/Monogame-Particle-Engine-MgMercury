b18b552bb56fb6cd07739155412c3280a9117f26
Marcelo B <marcelobelkiman@gmail.com>
2018-10-06 17:19:48 -0300

    new render method

--- Core/SpritebatchRenderer.cs
+++ Core/SpritebatchRenderer.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 
 namespace MonoGameMPE.Core
 {
@@ -9,19 +8,25 @@ namespace MonoGameMPE.Core
         /// <summary>
         /// Draw a particle effect. This draw function calls spritebatch.Begin() and .End()
         /// </summary>
-        public void Draw(ParticleEffect effect, SpriteBatch spriteBatch, Matrix cameraView, SpriteEffects effects, float layerDepth)
+        public void DrawComplete(ParticleEffect effect, SpriteBatch spriteBatch, Matrix cameraView, SpriteEffects effects, float layerDepth)
         {
             foreach (var emitter in effect.Emitters)
-                Draw(emitter.Value, spriteBatch, cameraView, effects, layerDepth);
+                DrawComplete(emitter.Value, spriteBatch, cameraView, effects, layerDepth);
         }
 
-        public void Draw(ParticleEffect effect, SpriteBatch s)
+        public void DrawComplete(ParticleEffect effect, SpriteBatch s)
         {
             foreach (var emitter in effect.Emitters)
-                Draw(emitter.Value, s);
+                DrawComplete(emitter.Value, s);
         }
 
-        private unsafe void Draw(Emitter emitter, SpriteBatch spriteBatch)
+        public void Draw(ParticleEffect effect, SpriteBatch spriteBatch, SpriteEffects effects, float layerDepth)
+        {
+            foreach (var emitter in effect.Emitters)
+                Draw(emitter.Value, spriteBatch, effects, layerDepth);
+        }
+
+        private unsafe void DrawComplete(Emitter emitter, SpriteBatch spriteBatch)
         {
             var texture = emitter.Texture;
             var origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
@@ -32,7 +37,7 @@ namespace MonoGameMPE.Core
 
             //TODO var sortMode = emitter.RenderingOrder == RenderingOrder.BackToFront ?
 
-            spriteBatch.Begin(SpriteSortMode.Deferred, blendState);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, blendState);
             {
                 var iterator = emitter.Buffer.Iterator;
                 while (iterator.HasNext)
@@ -60,7 +65,7 @@ namespace MonoGameMPE.Core
             spriteBatch.End();
         }
 
-        private unsafe void Draw(Emitter emitter, SpriteBatch spriteBatch, Matrix cameraView, SpriteEffects effects, float layerDepth)
+        private unsafe void DrawComplete(Emitter emitter, SpriteBatch spriteBatch, Matrix cameraView, SpriteEffects effects, float layerDepth)
         {
             var texture = emitter.Texture;
             var origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
@@ -71,7 +76,7 @@ namespace MonoGameMPE.Core
 
             //TODO var sortMode = emitter.RenderingOrder == RenderingOrder.BackToFront ?
 
-            spriteBatch.Begin(SpriteSortMode.Deferred, blendState, SamplerState.PointClamp, null, null, null, cameraView);
+            spriteBatch.Begin(SpriteSortMode.BackToFront, blendState, SamplerState.PointClamp, null, null, null, cameraView);
             {
                 var iterator = emitter.Buffer.Iterator;
                 while (iterator.HasNext)
@@ -94,6 +99,36 @@ namespace MonoGameMPE.Core
                 }
             }
             spriteBatch.End();
+        }
+
+        private unsafe void Draw(Emitter emitter, SpriteBatch spriteBatch, SpriteEffects effects, float layerDepth)
+        {
+            var texture = emitter.Texture;
+            var origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+            var blendState = emitter.BlendMode == BlendMode.Add
+                ? BlendState.Additive
+                : BlendState.AlphaBlend;
+
+            var iterator = emitter.Buffer.Iterator;
+            while (iterator.HasNext)
+            {
+                var particle = iterator.Next();
+                var color = particle->Colour.ToRgb();
+                if (blendState == BlendState.AlphaBlend) color *= particle->Opacity;
+                else color.A = (byte)(particle->Opacity * 255);
+
+                spriteBatch.Draw(
+                    texture,
+                    new Vector2(particle->Position.X, particle->Position.Y),
+                    null,
+                    new Color(color, particle->Opacity),
+                    particle->Rotation,
+                    origin,
+                    new Vector2(particle->Scale.X / texture.Width, particle->Scale.Y / texture.Height),
+                    effects,
+                    layerDepth);
+            }
         }
     }
 }
